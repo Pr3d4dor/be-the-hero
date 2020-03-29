e1a1b75327321eb84a3db58c0b293bc5f472f166
Gianluca Bine <gian_bine@hotmail.com>
2020-03-29 02:01:51 -0300

    Uso de middleware de erros de biblioteca de validação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 const listener = app.listen(process.env.PORT, () => {
     console.log('Listening on port ' + listener.address().port);
